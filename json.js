4bf3183c424b7a21a3cb53202e83fa63bca3882d
Krishna Suwal <krishnasuwal2019@gmail.com>
2022-03-12 18:50:07 +0545

    Json

---
+++ json.js
@@ -0,0 +1,14 @@
+const list = [
+  {
+    email: "ram@gmail.com",
+    username: 123,
+    saySomething() {
+      console.log("asdgwe");
+    },
+    age: 123,
+  },
+  {
+    email: "david@somthings",
+    username: "david",
+  },
+];
