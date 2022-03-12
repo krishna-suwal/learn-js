5d393441b0cb5130658046b428c784ce4a78fcc9
Shanesh Nepali <sns.themegrill@gmail.com>
2022-03-12 19:11:01 +0545

    json

--- json.js
+++ json.js
@@ -1,14 +1,44 @@
-const list = [
-  {
-    email: "ram@gmail.com",
-    username: 123,
-    saySomething() {
-      console.log("asdgwe");
-    },
-    age: 123,
-  },
-  {
-    email: "david@somthings",
-    username: "david",
-  },
-];
+// const list = [
+//   {
+//     email: "ram@gmail.com",
+//     username: 123,
+//     saySomething() {
+//       console.log("asdgwe");
+//     },
+//     age: 123,
+//   },
+//   {
+//     email: "david@somthings",
+//     username: "david",
+//   },
+// ];
+
+// const contact_list = [
+//   {
+//     name: "Krishna",
+//     number: 456879,
+//   },
+
+//   {
+//     name: "Dhrmu",
+//     number: 868979,
+//     email() {
+//       console.log("email@example.com");
+//     },
+//   },
+// ];
+
+// for (let i = 0; i < contact_list.length; i++) {
+//   console.log(contact_list[i].name);
+//   console.log(contact_list[i].number);
+
+// }
+// contact_list[i].email();
+// const a = {
+//   name: "Krishna",
+//   number: 456879,
+// };
+
+// console.log(a.name);
+
+//////////////////////////////////////////////
